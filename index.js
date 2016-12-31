64c51d497d2f9d9a4766ef279a3a795aeb25e4a8
Ashutosh Kumar Anand <ashutosh15018@iiitd.ac.in>
2016-12-31 22:18:00 +0530

    S Commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express')
 var app = express()
-var w3dInsert = require('./w3dInsert');
+
 
 app.get('/', function (req, res) {
   res.send('<form action="" method="post"><input type="text" name="col1"><input type="text" name="col2"><input type="text" name="col3"><input type="submit">');
@@ -12,4 +12,8 @@ app.post('/', function (req, res) {
   col3 = res.params.col3;
   col4 = "custom value";
   new w3dInsert(col1, col2, col3, col4);
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
 })
